9e3a2314a834da621f6d9175d0d0877edbc3f1d4
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-04-10 18:50:35 -0400

    Adds http origin for websocket..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.url = "ws://#{ENV["CABLE_URL"]}"
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [
+    'http://ankiachievements.com',
+    'https://ankiachievements.com'
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true

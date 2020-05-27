3d083da73337ae3fc6a03cba05926b70ac885c99
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-05-27 14:46:13 -0400

    Add mailer host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,6 +114,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.action_mailer.default_url_options = { host: 'https://ankiachievements.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
 

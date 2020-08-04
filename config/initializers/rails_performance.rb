008309e12cc3c25e8954541a5b406ecc72a5e511
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-08-04 12:10:07 -0400

    Increase rails performance monitoring time.

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -1,6 +1,6 @@
 RailsPerformance.setup do |config|
   config.redis    = Redis::Namespace.new("#{Rails.env}-rails-performance", redis: Redis.new)
-  config.duration = 60.minutes
+  config.duration = 120.minutes
 
   config.debug    = false # currently not used>
   config.enabled  = true

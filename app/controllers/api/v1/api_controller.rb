6eca769e1e3e44d1ae18449e9040831bb45dcb27
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-08-06 18:26:07 -0400

    Adds more logging around token users
    Now logs critical if the users do not match!

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class ApiController < ActionController::API
       include DeviseTokenAuth::Concerns::SetUserByToken
+      skip_after_action :update_auth_header
+      after_action :update_auth_header_and_log
 
       private
       # Trying to do this manually because we devise the wrong user sometimes!!!
@@ -14,14 +16,25 @@ module Api
 
       def current_token_user
         @current_token_user ||= set_user_by_token.tap do |user|
-          Rails.logger.info "HEADERS uid: #{request.headers["uid"]}"
+          Rails.logger.info "REQUEST HEADERS uid: #{request.headers["uid"]}"
           Rails.logger.info "set user uid: #{user.uid}"
           Rails.logger.info "set user email: #{user.email}"
+          if request.headers["uid"] != user.uid
+            Rails.logger.critical "WRONG USER LOADED!"
+          end
         end
       end
 
       def resource_name
         "User"
+      end
+
+      def update_auth_header_and_log
+        update_auth_header()
+        Rails.logger.info "RESPONSE HEADERS uid: #{response.headers["uid"]}"
+        if request.headers["uid"] != response.headers["uid"]
+          Rails.logger.critical "WRONG USER RETURNED!"
+        end
       end
     end
   end

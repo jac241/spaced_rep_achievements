118f7cd1639e8cd03aae28ce5a156922855c1462
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-10-09 12:06:43 -0400

    Removes old debug logging, readds rails_perf

--- Gemfile
+++ Gemfile
@@ -50,6 +50,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :development, :production do
+  gem 'rails_performance'
+end
+
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,8 +2,6 @@ module Api
   module V1
     class ApiController < ActionController::API
       include DeviseTokenAuth::Concerns::SetUserByToken
-      skip_after_action :update_auth_header
-      after_action :update_auth_header_and_log
 
       private
       # Trying to do this manually because we devise the wrong user sometimes!!!
@@ -15,26 +13,11 @@ module Api
       end
 
       def current_token_user
-        @current_token_user ||= set_user_by_token.tap do |user|
-          Rails.logger.info "REQUEST HEADERS uid: #{request.headers["uid"]}"
-          Rails.logger.info "set user uid: #{user.uid}"
-          Rails.logger.info "set user email: #{user.email}"
-          if request.headers["uid"] != user.uid
-            Rails.logger.info "WRONG USER LOADED!"
-          end
-        end
+        @current_token_user ||= set_user_by_token
       end
 
       def resource_name
         "User"
-      end
-
-      def update_auth_header_and_log
-        update_auth_header()
-        Rails.logger.info "RESPONSE HEADERS uid: #{response.headers["uid"]}"
-        if request.headers["uid"] != response.headers["uid"]
-          Rails.logger.info "WRONG USER RETURNED!"
-        end
       end
     end
   end

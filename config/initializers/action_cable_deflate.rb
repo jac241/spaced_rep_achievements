110a59ef1734e945641b90ab65852ad38a59395d
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-11-26 20:00:22 -0500

    Actually adds permessage-deflate to ws

--- config/initializers/action_cable_deflate.rb
+++ config/initializers/action_cable_deflate.rb
@@ -1,11 +1,12 @@
-module Connection
-  class ClientSocket
-    alias_method :old_initialize, :initialize
+module ActionCable
+  module Connection
+    class ClientSocket
+      alias_method :old_initialize, :initialize
 
-    def initialize(env, event_target, event_loop, protocols)
-      old_initialize(env, event_target, event_loop, protocols)
-      @driver.add_extension(PermessageDeflate)
+      def initialize(env, event_target, event_loop, protocols)
+        old_initialize(env, event_target, event_loop, protocols)
+        @driver.add_extension(PermessageDeflate)
+      end
     end
-
   end
 end

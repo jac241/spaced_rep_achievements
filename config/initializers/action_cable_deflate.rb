508a0fd7f73b3f570395727159b41a51db3973df
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-11-26 23:02:46 -0500

    Removes bytesize logging. convince deflating

--- config/initializers/action_cable_deflate.rb
+++ config/initializers/action_cable_deflate.rb
@@ -2,16 +2,10 @@ module ActionCable
   module Connection
     class ClientSocket
       alias_method :old_initialize, :initialize
-      alias_method :old_write, :write
 
       def initialize(env, event_target, event_loop, protocols)
         old_initialize(env, event_target, event_loop, protocols)
         @driver.add_extension(PermessageDeflate)
-      end
-
-      def write(data)
-        old_write(data)
-        Rails.logger.info "broadcasting: #{data.bytesize} bytes"
       end
     end
   end

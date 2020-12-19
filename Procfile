05112aed192b229ba5dd92742d9809f3ee457bc1
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-12-18 22:39:24 -0500

    Putting anycable back in...

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 web: bundle exec rails server
 acgrpc: bundle exec anycable
-acws: anycable-go
+acws: anycable-go --enable_ws_compression
 worker: bundle exec sidekiq
 release: bin/rails db:migrate:with_data
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Specify AnyCable WebSocket server URL to use by JS client
+  config.after_initialize do
+    config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL", "ws://localhost:8080/cable") if AnyCable::Rails.enabled?
+  end
   config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
   # Settings specified here will take precedence over those in config/application.rb.
 

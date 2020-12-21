6d03787003d05b199dc97c3ed76a0c3fd10237d7
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-12-21 13:24:15 -0500

    Removes console.log

--- app/javascript/realtime_leaderboard/reducerUtil.js
+++ app/javascript/realtime_leaderboard/reducerUtil.js
@@ -4,7 +4,6 @@ export const upsertIfUpdated = (state, entityAdapter, incomingEntity) => {
   if (existing === undefined) {
     entityAdapter.upsertOne(state, incomingEntity)
   } else if (new Date(incomingEntity.attributes.updatedAt) > new Date(existing.attributes.updatedAt)) {
-    console.log("newer, upserting")
     entityAdapter.upsertOne(state, incomingEntity)
   }
 }

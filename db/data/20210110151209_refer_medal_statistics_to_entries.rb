48c7401a2c30e768ea51f7ea1205b95ebc8ab6c5
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2021-01-10 11:11:45 -0500

    updates migration to work on remote

--- db/data/20210110151209_refer_medal_statistics_to_entries.rb
+++ db/data/20210110151209_refer_medal_statistics_to_entries.rb
@@ -2,8 +2,8 @@ class ReferMedalStatisticsToEntries < ActiveRecord::Migration[6.0]
   def up
     Entry.includes(:reified_leaderboard, :user).find_each do |entry|
       MedalStatistic.where(
-        reified_leaderboard: entry.reified_leaderboard,
-        user: entry.user,
+        reified_leaderboard_id: entry.reified_leaderboard.id,
+        user_id: entry.user_id,
       ).update_all(entry_id: entry.id)
     end
   end

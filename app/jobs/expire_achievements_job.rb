9b8a486f176f5efd7039d2c728959a22f8d2a472
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2021-02-06 09:51:42 -0500

    Improve logging on Expire Achievements Job

--- app/jobs/expire_achievements_job.rb
+++ app/jobs/expire_achievements_job.rb
@@ -2,63 +2,70 @@ class ExpireAchievementsJob < ApplicationJob
   queue_as :realtime_leaderboard
 
   def perform
-    ReifiedLeaderboard.find_each do |leaderboard|
-      Rails.logger.info("Expiring for leaderboard: #{leaderboard.family.name} #{leaderboard.timeframe}")
-      entries_cache, medal_stats_cache = ApplicationRecord.transaction do
-        expirations =
-          Expiration
-            .expired_for_leaderboard(leaderboard)
+    old_log_level = Rails.logger.level
+    begin
+      Rails.logger.level = 0 # Debug
+      ReifiedLeaderboard.find_each do |leaderboard|
+        Rails.logger.info("Expiring for leaderboard: #{leaderboard.family.name} #{leaderboard.timeframe}")
+        entries_cache, medal_stats_cache = ApplicationRecord.transaction do
+          expirations =
+            Expiration
+              .expired_for_leaderboard(leaderboard)
 
-        affected_entries = Entry
-          .includes(:medal_statistics)
-          .where(
-            reified_leaderboard: leaderboard,
-            user_id: Achievement.where(
-              id: expirations.pluck(:achievement_id)).distinct.pluck(:user_id)
-          )
+          affected_entries = Entry
+            .includes(:medal_statistics)
+            .where(
+              reified_leaderboard: leaderboard,
+              user_id: Achievement.where(
+                id: expirations.pluck(:achievement_id)).distinct.pluck(:user_id)
+            )
 
-        entries_cache =
-          affected_entries
-            .group_by { |e| [e.reified_leaderboard, e.user_id] }
-            .transform_values { |v| v.first }
+          entries_cache =
+            affected_entries
+              .group_by { |e| [e.reified_leaderboard, e.user_id] }
+              .transform_values { |v| v.first }
 
-        medal_stats_cache =
-          MedalStatistic.where(entry: affected_entries.pluck(:id))
-            .group_by { |m| [m.entry, m.medal_id] }
-            .transform_values { |v| v.first }
+          medal_stats_cache =
+            MedalStatistic.where(entry: affected_entries.pluck(:id))
+              .group_by { |m| [m.entry, m.medal_id] }
+              .transform_values { |v| v.first }
 
-        bm = Benchmark.measure do
-          expirations.find_each { |exp| exp.expire(entries_cache, medal_stats_cache) }
+          bm = Benchmark.measure do
+            expirations.find_each { |exp| exp.expire(entries_cache, medal_stats_cache) }
+          end
+          Rails.logger.info("Time to expire records: #{1000 * bm.real}ms")
+
+          expired_count = nil
+          bm = Benchmark.measure do
+            expired_count = expirations.delete_all
+          end
+          Rails.logger.info("Number of Expirations: #{expired_count}")
+          Rails.logger.info("Time to delete_all expirations: #{1000 * bm.real}ms")
+
+          Rails.logger.info("Number of entries affected: #{entries_cache.size}")
+          Rails.logger.info("Number of medal stats affected: #{medal_stats_cache.size}")
+
+          bm = Benchmark.measure do
+            entries_cache.values.each(&:save!)
+          end
+          Rails.logger.info("Time to save entries: #{1000 * bm.real}ms")
+
+          bm = Benchmark.measure do
+            medal_stats_cache.values.each(&:save!)
+          end
+          Rails.logger.info("Time to save medal stats: #{1000 * bm.real}ms")
+
+          [entries_cache, medal_stats_cache]
         end
-        Rails.logger.info("Time to expire records: #{1000 * bm.real}ms")
 
-        expired_count = nil
-        bm = Benchmark.measure do
-          expired_count = expirations.delete_all
-        end
-        Rails.logger.info("Number of Expirations: #{expired_count}")
-        Rails.logger.info("Time to delete_all expirations: #{1000 * bm.real}ms")
-
-        Rails.logger.info("Number of entries affected: #{entries_cache.size}")
-        Rails.logger.info("Number of medal stats affected: #{medal_stats_cache.size}")
-
-        bm = Benchmark.measure do
-          entries_cache.values.each(&:save!)
-        end
-        Rails.logger.info("Time to save entries: #{1000 * bm.real}ms")
-
-        bm = Benchmark.measure do
-          medal_stats_cache.values.each(&:save!)
-        end
-        Rails.logger.info("Time to save medal stats: #{1000 * bm.real}ms")
-
-        [entries_cache, medal_stats_cache]
+        BroadcastLeaderboardChangesetService.call(
+          entries: entries_cache.values,
+          medal_statistics: medal_stats_cache.values,
+        )
       end
-
-      BroadcastLeaderboardChangesetService.call(
-        entries: entries_cache.values,
-        medal_statistics: medal_stats_cache.values,
-      )
+    ensure
+      Rails.logger.level = old_log_level
+      Rails.logger.info("Rails log level reset!")
     end
   end
 end

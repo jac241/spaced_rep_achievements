535f23308f0a55747786b6b50543fbbeef19aeb4
James Castiglione <jac241@users.noreply.github.com>
2021-08-28 16:22:34 -0400

    Makes medal stats and entries caches store results

--- app/models/expiration.rb
+++ app/models/expiration.rb
@@ -10,21 +10,23 @@ class Expiration < ApplicationRecord
 
   def expire(entries_cache, medal_stats_cache)
     #entry = entries_cache[[reified_leaderboard_id, achievement.user_id]]
-    entry = entries_cache.fetch([reified_leaderboard_id, achievement.user_id]) do
+    entry = entries_cache.fetch([reified_leaderboard_id, achievement.user_id]) do |key|
       Rails.logger.info "Could not find entry in cache for RlbID: #{reified_leaderboard_id}, user_id: #{achievement.user_id}"
-      Entry.find_by(
+      e = Entry.find_by(
         reified_leaderboard_id: reified_leaderboard_id,
         user_id: achievement.user_id
       )
+      entries_cache[key] = e
     end
     entry.instance_score_delta -= self.points
 
-    stats = medal_stats_cache.fetch([entry.id, achievement.medal_id]) do
+    stats = medal_stats_cache.fetch([entry.id, achievement.medal_id]) do |key|
       Rails.logger.info "Could not find medal stat in cache for entry_id: #{entry.id}, medal: #{achievement.medal_id}"
-      MedalStatistic.find_by(
+      ms = MedalStatistic.find_by(
         entry_id: entry.id,
         medal_id: achievement.medal_id,
       )
+      medal_stats_cache[key] = ms
     end
     stats.tally_medal_removal(achievement.medal.score)
 

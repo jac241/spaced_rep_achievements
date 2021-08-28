85b5adc70fd17461298e42471bc37144fabd9f35
James Castiglione <jac241@users.noreply.github.com>
2021-08-28 15:03:36 -0400

    Find medal stats if missing

--- app/models/expiration.rb
+++ app/models/expiration.rb
@@ -11,6 +11,7 @@ class Expiration < ApplicationRecord
   def expire(entries_cache, medal_stats_cache)
     #entry = entries_cache[[reified_leaderboard_id, achievement.user_id]]
     entry = entries_cache.fetch([reified_leaderboard_id, achievement.user_id]) do
+      Rails.logger.info "Could not find entry in cache for RlbID: #{reified_leaderboard_id}, user_id: #{achievement.user_id}"
       Entry.find_by(
         reified_leaderboard_id: reified_leaderboard_id,
         user_id: achievement.user_id
@@ -18,7 +19,13 @@ class Expiration < ApplicationRecord
     end
     entry.instance_score_delta -= self.points
 
-    stats = medal_stats_cache[[entry.id, achievement.medal_id]]
+    stats = medal_stats_cache.fetch([entry.id, achievement.medal_id]) do
+      Rails.logger.info "Could not find medal stat in cache for entry_id: #{entry.id}, medal: #{achievement.medal_id}"
+      MedalStatistic.find_by(
+        entry_id: entry.id,
+        medal_id: achievement.medal_id,
+      )
+    end
     stats.tally_medal_removal(achievement.medal.score)
 
     self
